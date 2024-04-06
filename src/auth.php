40df8b407c100faae81dc640ca3b956a0b60382c
iballi2017 <iballi201@gmail.com>
2024-04-06 23:12:44 +0100

    development

---
+++ src/auth.php
@@ -0,0 +1,21 @@
+<!-- header -->
+<?php include_once("./components/header.php"); ?>
+
+<div class="mx-auto min-h-screen flex items-center justify-center">
+    <div class="mx-auto w-full max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div class="block mx-auto w-full max-w-5xl bg-white border border-gray-200 rounded-lg overflow-hidden shadow dark:bg-gray-800 dark:border-gray-700">
+            <div class="grid grid-cols-2">
+                <div class="hidden sm:block col-span-2 sm:col-span-1 bg-dark-room-surgeon">
+                    ddd
+                </div>
+                <div class="col-span-2 sm:col-span-1 p-6">
+                    <h1 class="text-center">Sign Into Your Account</h1>
+                    <p class="text-center">Use your email to login</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- footer -->
+<?php include_once("./components/footer.php"); ?>
---
+++ src/components/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <!-- tailwindcss -->
+    <link rel="stylesheet" href="./stylesheets/tailwindcss/output.css" />
+    <!-- custom styles -->
+    <link rel="stylesheet" href="./stylesheets/main.css" />
+</head>
+
+<body>
+
+    <!-- header -->
