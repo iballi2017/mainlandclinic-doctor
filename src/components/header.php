13c18724a47876e1cbd885f01247218ad12c1c25
iballidev <iballi.dev@gmail.com>
2024-04-29 17:38:01 +0100

    style datepicker

--- src/components/header.php
+++ src/components/header.php
@@ -7,9 +7,11 @@
     <title>Mainlandclinics | Doctor</title>
     <!-- fontawesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css" integrity="sha512-q3eWabyZPc1XTCmF+8/LuE1ozpg5xxn7iO89yfSOd5/oKvyqLngoNGsx8jq92Y8eXJ/IRxQbEC+FGSYxtk2oiw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-   
+
     <!-- tailwindcss -->
     <link rel="stylesheet" href="./stylesheets/tailwindcss/output.css" />
+    <!-- jQuery UI library -->
+    <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css" />
     <!-- custom styles -->
     <link rel="stylesheet" href="./stylesheets/main.css" />
 </head>
